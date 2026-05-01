dd40db960757c5be3eef34e7a095eda26f814caa
AwsQuer <quer1999@gmail.com>
2026-05-01 18:30:13 +0200

    fix: handle DB connection error and S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "aws-fullstack-app-tfstate-863430823865"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
